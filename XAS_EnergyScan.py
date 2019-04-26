19ad08308cca6518e6c634609e8e3bdf187110b2
Chelsea L-S <chelsea738@gmail.com>
2019-04-26 10:10:35 -0700

    Filters are in now, looking for optimal values

--- XAS_EnergyScan.py
+++ XAS_EnergyScan.py
@@ -12,109 +12,173 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import compress
-from matplotlib.widgets import Cursor
 import math
 import statistics as stat
 
+ReEnterData = False
+
 #Set up the scans and the number of time steps
-FileNums = list(range(371,371+1))
 
-NumTTSteps_ES = 1
-NumTTStepsPlots_ES = NumTTSteps_ES
+NumTTSteps = 6
+NumTTStepsPlots = NumTTSteps
 
+if ReEnterData:
 
-#Initialize the necessary lists
-XOn = []
-LOn = []
-XEnergy = []
-TimeTool = []
-Diode2 = []
-
-Ipm2Sum = []
-Ipm2Median = []
-Ipm2STD = []
-DiodeIpmSlope = []
-DISMedian = []
-DISSTD = []
-
-CspadSum = []
-CspadMedian = []
-CspadSTD = []
-DiodeCspadSlope = []
-DCSMedian = []
-DCSSTD = []
-
-ScanNum = []
-
-
-#Fill the lists with data from the h5 file
-for filenum in FileNums:
+    FileNums = list(range(371,395+1))
     
-    ScanName = h5py.File('Data\ldat_xppj6715_Run' + str(filenum) + '.h5')
+    #Initialize the necessary lists
+    XOn = []
+    LOn = []
+    XEnergy = []
+    Diode2 = []
     
-    xOn = list(map(bool, ScanName['/lightStatus/xray']))
-    XOn = XOn + xOn
-    LOn = LOn + list(map(bool, ScanName['/lightStatus/laser']))
-    XEnergy = XEnergy + [round(2*x,3)/2 for x in list(ScanName['/scan/var0'])]
-    TimeTool = TimeTool + list(ScanName['/ttCorr/tt'])
-    diode = [x[2] for x in list(ScanName['/diodeU/channels'])]                  #Quad cell 2 from diode - this one has an output
-    Diode2 = Diode2 + diode
-   
-    ipm2 = [float(x[1])+float(x[3]) for x in list(ScanName['/ipm2/channels'])]  #Intensity (and position) monitor #2.  Quad cells 1 and 3 had signal - use these
-    Ipm2Sum = Ipm2Sum + ipm2
-    statmedium = stat.median(compress(ipm2, xOn))
-    Ipm2Median = Ipm2Median + [float(statmedium) for x in range(len(ipm2))]
-    statstdev = stat.stdev(compress(ipm2, xOn))
-    Ipm2STD = Ipm2STD + [float(statstdev) for x in range(len(ipm2))]
-    
-    slope = [y/x for x,y in zip(diode, ipm2)]
-    DiodeIpmSlope = DiodeIpmSlope + slope
-    statmedium = stat.median([x for x in slope if not math.isnan(x)])
-    DISMedian = [float(statmedium) for x in range(len(ipm2))]
-    statstdev = stat.stdev([i-statmedium*d for d,i in zip(diode, ipm2)])
-    DISSTD = DISSTD + [float(statstdev) for x in range(len(ipm2))]
-    
-    Cspad = []
-    for cspad in list(ScanName['/cspad/azav']):
-        Cspad.append(float(sum([x for x in cspad if not math.isnan(x)])))
+    Ipm2Sum = []
+    Ipm2Median = []
+    Ipm2STD = []
+    DiodeIpmSlope = []
+    DISMedian = []
+    DISSTD = []
         
-    CspadSum = CspadSum + Cspad
-    statmedium = stat.median(Cspad)
-    CspadMedian = CspadMedian + [float(statmedium) for x in range(len(Cspad))]
-    statstdev = stat.stdev(Cspad)
-    CspadSTD = CspadSTD + [float(statstdev) for x in range(len(Cspad))]
+    CspadSum = []
+    CspadMedian = []
+    CspadSTD = []
+    DiodeCspadSlope = []
+    DCSMedian = []
+    DCSSTD = []
     
-    slope = [y/x for x,y in zip(diode, Cspad)]
-    DiodeCspadSlope = DiodeCspadSlope + slope
-    statmedium = stat.median(slope)
-    DCSMedian = [float(statmedium) for x in range(len(diode))]
-    statstdev = stat.stdev([c-statmedium*d for d,c in zip(diode, Cspad)])
-    DCSSTD = DISSTD + [float(statstdev) for x in range(len(diode))]
+    TimeTool = []
+    TTMedian = []
+    TTSTD = []
+    TTAmp = []
+    TTAmpMedian = []
+    TTAmpSTD = []
+    TTFWHM = []
+    TTFWHMMedian = []
+    TTFWHMSTD = []
     
-    ScanNum = ScanNum + [filenum for x in range(len(diode))]
+    ScanNum = []
+    
 
+    #Fill the lists with data from the h5 file
+    for filenum in FileNums:
+        
+        ScanName = h5py.File('Data\ldat_xppj6715_Run' + str(filenum) + '.h5')
+        
+        xOn = list(map(bool, ScanName['/lightStatus/xray']))
+        XOn = XOn + xOn
+        LOn = LOn + list(map(bool, ScanName['/lightStatus/laser']))
+        XEnergy = XEnergy + [round(4*x,3)/4 for x in list(ScanName['/scan/var0'])]
 
-#Convert the timetool signal into femtosecond delays and create the time slices
-TTDelay_ES = [x*1000 for x in TimeTool]
-TTSteps = np.linspace(min(TTDelay_ES),max(TTDelay_ES),NumTTSteps_ES+1)
+        diode = [x[2] for x in list(ScanName['/diodeU/channels'])]                  #Quad cell 2 from diode - this one has an output
+        Diode2 = Diode2 + diode
+        
+        ipm2 = [float(x[1])+float(x[3]) for x in list(ScanName['/ipm2/channels'])]  #Intensity (and position) monitor #2.  Quad cells 1 and 3 had signal - use these
+        Ipm2Sum = Ipm2Sum + ipm2
+        statmedian = stat.median(compress(ipm2, xOn))
+        Ipm2Median = Ipm2Median + [float(statmedian) for x in range(len(ipm2))]
+        statstdev = stat.stdev(compress(ipm2, xOn))
+        Ipm2STD = Ipm2STD + [float(statstdev) for x in range(len(ipm2))]
+        
+        slope = [y/x for x,y in zip(diode, ipm2)]
+        DiodeIpmSlope = DiodeIpmSlope + slope
+        statmedian = stat.median([x for x in slope if not math.isnan(x)])
+        DISMedian = [float(statmedian) for x in range(len(ipm2))]
+        statstdev = stat.stdev([i-statmedian*d for d,i in zip(diode, ipm2)])
+        DISSTD = DISSTD + [float(statstdev) for x in range(len(ipm2))]
+        
+        Cspad = []
+        for cspad in list(ScanName['/cspad/azav']):
+            Cspad.append(float(sum([x for x in cspad if not math.isnan(x)])))
+            
+        CspadSum = CspadSum + Cspad
+        statmediun = stat.median(Cspad)
+        CspadMedian = CspadMedian + [float(statmedian) for x in range(len(Cspad))]
+        statstdev = stat.stdev(Cspad)
+        CspadSTD = CspadSTD + [float(statstdev) for x in range(len(Cspad))]
+        
+        slope = [y/x for x,y in zip(diode, Cspad)]
+        DiodeCspadSlope = DiodeCspadSlope + slope
+        statmedian = stat.median(slope)
+        DCSMedian = [float(statmedian) for x in range(len(diode))]
+        statstdev = stat.stdev([c-statmedian*d for d,c in zip(diode, Cspad)])
+        DCSSTD = DCSSTD + [float(statstdev) for x in range(len(diode))]
+        
+        timetool = [float(x) for x in list(ScanName['/ttCorr/tt'])]
+        TimeTool = TimeTool + timetool
+        
+        statmedian = stat.median(compress(timetool, [a and b for a,b in zip(LOn, XOn)]))
+        TTMedian = TTMedian + [float(statmedian) for x in range(len(timetool))]
+        statstdev = stat.stdev(compress(timetool, [a and b for a,b in zip(LOn, XOn)]))
+        TTSTD = TTSTD + [float(statstdev) for x in range(len(timetool))]
+        
+        timetoolamp = [float(x) for x in list(ScanName['/tt/XPP_TIMETOOL_AMPL'])]
+        TTAmp = TTAmp + timetoolamp
+        
+        statmedian = stat.median(compress(timetoolamp, [a and b for a,b in zip(LOn, XOn)]))
+        TTAmpMedian = TTAmpMedian + [float(statmedian) for x in range(len(timetoolamp))]
+        statstdev = stat.stdev(compress(timetoolamp, [a and b for a,b in zip(LOn, XOn)]))
+        TTAmpSTD = TTAmpSTD + [float(statstdev) for x in range(len(timetoolamp))]
+        
+        timetoolfwhm = [float(x) for x in list(ScanName['/tt/XPP_TIMETOOL_AMPL'])]
+        TTFWHM = TTFWHM + timetoolfwhm
+        
+        statmedian = stat.median(compress(timetoolfwhm, [a and b for a,b in zip(LOn, XOn)]))
+        TTFWHMMedian = TTFWHMMedian + [float(statmedian) for x in range(len(timetoolfwhm))]
+        statstdev = stat.stdev(compress(timetoolfwhm, [a and b for a,b in zip(LOn, XOn)]))
+        TTFWHMSTD = TTFWHMSTD + [float(statstdev) for x in range(len(timetoolfwhm))]
+        
+        ScanNum = ScanNum + [filenum for x in range(len(diode))]
 
 
 #Set up the intensity filter - the diode and cspad sum should respond linearly with the x-ray intensity
-
-IpmNumSTDs = 1
+IpmNumSTDs = 3
 IpmFilter = list(a < b+IpmNumSTDs*c and a > b-IpmNumSTDs*c for a,b,c in zip(Ipm2Sum, Ipm2Median, Ipm2STD))
 
-DISSTDs = 1
+DISSTDs = 1.5
 DISFilter = list(a < b*c+DISSTDs*d and a > b*c-DISSTDs*d for a,b,c,d in zip(Ipm2Sum, Diode2, DISMedian, DISSTD))
 
-CspadSTDs = 1
-CspadFilter = list(a < b+CspadSTD*c and a > b+CspadSTD*c for a,b,c in zip(CspadSum, CspadMedian, CspadSTD))
+CspadSTDs = 3
+CspadFilter = list(a < b+CspadSTDs*c and a > b-CspadSTDs*c for a,b,c in zip(CspadSum, CspadMedian, CspadSTD))
 
-DCSSTDs = 1
+DCSSTDs = 1.5
 DCSFilter = list(a < b*c+DCSSTDs*d and a > b*c-DCSSTDs*d for a,b,c,d in zip(CspadSum, Diode2, DCSMedian, DCSSTD))
 
-IntensityFilter = list(a and b and c and d for a,b,c,d in zip(IpmFilter, DISFilter, CspadFilter, DCSFilter))
+#IntensityFilter = list(a for a in zip(IpmFilter))
+IntensityFilter = list((a and b and c and d) or not e for a,b,c,d,e in zip(IpmFilter, DISFilter, CspadFilter, DCSFilter, XOn))
 
+plt.figure()
+plt.scatter(Diode2, Ipm2Sum, s=1)
+plt.scatter(list(compress(Diode2, IntensityFilter)), list(compress(Ipm2Sum, IntensityFilter)), s=1, alpha = .25)
+plt.title('intensity filter effect')
+plt.xlabel('diode signal')
+plt.ylabel('intensity signal')
+
+
+#Convert the timetool signal into femtosecond delays and create the time tool filters
+TTSTDs = 2
+TTValueFilter = list(a < b+TTSTDs*c and a > b-TTSTDs*c for a,b,c in zip(TimeTool, TTMedian, TTSTD))
+
+TTAmpSTDs = 2
+TTAmpFilter = list(a < b+TTAmpSTDs*c and a > b-TTAmpSTDs*c for a,b,c in zip(TTAmp, TTAmpMedian, TTAmpSTD))
+
+TTFWHMSTDs = 2
+TTFWHMFilter = list(a < b+TTFWHMSTDs*c and a > b-TTFWHMSTDs*c for a,b,c in zip(TTFWHM, TTFWHMMedian, TTFWHMSTD))
+
+TTFilter = list((a and b and c) or not d or not e for a,b,c,d,e in zip(TTValueFilter, TTAmpFilter, TTFWHMFilter, XOn, LOn))
+
+TTDelay = [x*1000 for x in TimeTool]
+TTSteps = np.array([float(min(TTDelay)), float(-100), float(-50), float(0), float(50), float(100), float(max(TTDelay))])
+#TTSteps = np.linspace(min(TTDelay),max(TTDelay),NumTTSteps+1)
+
+fig=plt.figure()
+fig.add_subplot(121)
+plt.hist(list(compress(TTDelay, [a and b for a,b in zip(XOn, LOn)])), 1000)
+plt.title('time tool before filters')
+fig.add_subplot(122)
+plt.hist(list(compress(TTDelay, [a and b and c for a,b,c in zip(TTFilter, XOn, LOn)])), 1000)
+plt.title('time tool after filters')
+
+#Make XAS spectra
 UniXEnergy = np.unique(XEnergy)
 
 XASOn = [[0 for x in range(len(UniXEnergy))] for y in range(len(TTSteps)-1)]
@@ -123,70 +187,71 @@ XASOff = [0 for x in range(len(UniXEnergy))]
 On_NumScan = [[0 for x in range(len(UniXEnergy))] for y in range(len(TTSteps)-1)]
 Off_NumScan = [0 for x in range(len(UniXEnergy))]
 
+NormFactor_On = [[0 for x in range(len(UniXEnergy))] for y in range(len(TTSteps)-1)]
+NormFactor_Off = [0 for x in range(len(UniXEnergy))]
+
 for jj in range(len(UniXEnergy)):
     
-    SelectedRuns = list(a and b and not c for a,b,c in zip(XOn, (XEnergy == UniXEnergy[jj]), [math.isnan(x) for x in Diode2]))
+    SelectedRuns = list(a and b and not c and d and e for a,b,c,d,e in zip(XOn, (XEnergy == UniXEnergy[jj]), [math.isnan(x) for x in Diode2], IntensityFilter, TTFilter))
     
-    Off = list(not a and b for a,b in zip(LOn, SelectedRuns))
-    XASOff[jj] = sum(list(compress(Diode2, Off)))
+    off = list(not a and b for a,b in zip(LOn, SelectedRuns))
+    XASOff[jj] = sum(list(compress(Diode2, off)))
     
-    Off_NumScan[jj] = sum([int(x) for x in Off])
+    Off_NumScan[jj] = sum([int(x) for x in off])
+    NormFactor_Off[jj] = sum(list(compress(CspadSum, off)))
     
     for ii in range(len(TTSteps)-1):
         
-        On = list(a and b and c and d for a,b,c,d in zip(LOn, SelectedRuns, (TTDelay_ES >= TTSteps[ii]), (TTDelay_ES < TTSteps[ii+1])))
-        XASOn[ii][jj] = sum(list(compress(Diode2, On)))
+        on = list(a and b and c and d for a,b,c,d in zip(LOn, SelectedRuns, (TTDelay >= TTSteps[ii]), (TTDelay < TTSteps[ii+1])))
+        XASOn[ii][jj] = sum(list(compress(Diode2, on)))
         
-        On_NumScan[ii][jj] = sum([int(x) for x in On])
+        On_NumScan[ii][jj] = sum([int(x) for x in on])
+        NormFactor_On[ii][jj] = sum(list(compress(CspadSum, on)))
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
 
 XASOn_Norm = [[0 for x in range(len(UniXEnergy))] for y in range(len(TTSteps)-1)]
 
-for ii in range(NumTTStepsPlots_ES):
+AtleastOne = [x > 0 for x in Off_NumScan]
 
-    XASOn_Norm[ii] = [a/b for a,b in zip(XASOn[ii], On_NumScan[ii])]
-    plt.plot(UniXEnergy, XASOn_Norm[ii])
+for ii in range(NumTTStepsPlots):
+    AtleastOne = [a and b > 0 for a,b in zip(AtleastOne, On_NumScan[ii])]
+
+LegendLabel = []
+
+for ii in range(NumTTStepsPlots):
+
+    XASOn_Norm[ii] = [a/b for a,b,c in zip(XASOn[ii], NormFactor_On[ii], AtleastOne) if c]
+    LegendLabel = LegendLabel + plt.plot(list(compress(UniXEnergy, [x > 0 for x in On_NumScan[ii]])), XASOn_Norm[ii])
     
 plt.xlabel('x-ray energy (keV)')
 plt.ylabel('x-ray absorption')
-plt.title('Laser On')
-cursor = Cursor(ax, useblit=True, color='red', linewidth=2)
 
-XASOff_Norm = [a/b for a,b in zip(XASOff, Off_NumScan)]
+XASOff_Norm = [a/b for a,b,c in zip(XASOff, NormFactor_Off, AtleastOne) if c]
 
-plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(UniXEnergy, XASOff_Norm)
+LegendLabel = LegendLabel + plt.plot(list(compress(UniXEnergy, [x > 0 for x in Off_NumScan])), XASOff_Norm)
 plt.xlabel('x-ray energy (keV)')
 plt.ylabel('x-ray absorption')
-plt.title('Laser Off')
-cursor = Cursor(ax, useblit=True, color='red', linewidth=2)
 
+LegendWords = []
+for ii in range(NumTTStepsPlots):
+    LegendWords = LegendWords + [str(round(TTSteps[ii],0)) + ' to ' + str(round(TTSteps[ii+1],0)) + ' fs delay']
+
+LegendWords = LegendWords + ['Off']
+
+plt.legend(LegendLabel, LegendWords)
+
+LegendLabel = []
 plt.figure()
-ax = fig.add_subplot(111)
-
-for ii in range(NumTTStepsPlots_ES):
+for ii in range(NumTTStepsPlots):
     
-    plt.plot(UniXEnergy, [a - b for a,b, in zip(XASOn_Norm[ii], XASOff_Norm)])
+    LegendLabel = LegendLabel + plt.plot(list(compress(UniXEnergy, [x > 0 and y > 0 for x,y in zip(On_NumScan[ii], AtleastOne)])), [a - b for a,b in zip(XASOn_Norm[ii], XASOff_Norm)])
     #plt.plot(UniXEnergy, [a/sum(XESOn[ii]) - b/sum(XESOff) for a,b, in zip(XESOn[ii], XESOff)])
     
 plt.xlabel('x-ray energy (keV)')
 plt.ylabel('difference in x-ray absorption (on-off)')
 plt.title('raw data')
-cursor = Cursor(ax, useblit=True, color='red', linewidth=2)
 
-plt.figure()
-ax = fig.add_subplot(111)
+LegendWords.pop()
 
-for ii in range(NumTTStepsPlots_ES):
-    
-    plt.plot(UniXEnergy, [a/sum(XASOn_Norm[ii]) - b/sum(XASOff_Norm) for a,b, in zip(XASOn_Norm[ii], XASOff_Norm)])
-    #plt.plot(UniXEnergy, [a/sum(XESOn[ii]) - b/sum(XESOff) for a,b, in zip(XESOn[ii], XESOff)])
-    
-plt.xlabel('x-ray energy (keV)')
-plt.ylabel('difference in x-ray absorption (on-off), extra norm')
-plt.title('raw data')
-cursor = Cursor(ax, useblit=True, color='red', linewidth=2)
-
+plt.legend(LegendLabel, LegendWords)
