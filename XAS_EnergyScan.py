16e04d0e75f6c169b00a66b0140ba5a15ab5a520
Chelsea L-S <chelsea738@gmail.com>
2019-05-09 17:59:12 -0700

    XAS plot is looking good now.  Time for details, like picking the times points, automating t0, and the energy calibration

--- XAS_EnergyScan.py
+++ XAS_EnergyScan.py
@@ -22,7 +22,7 @@ FPlots = False
 
 #Set up the scans and the number of time steps
 
-NumTTSteps = 20
+NumTTSteps = 3
 NumTTStepsPlots = 2
 
 if ReEnterData:
@@ -38,7 +38,7 @@ t0 = -50
 
 TTDelay = [x*1000 - t0 for x in TimeTool]
 #TTSteps = np.array([float(-500), float(0), float(500)])
-TTSteps = np.linspace(-1000,1000,NumTTSteps+1)
+TTSteps = np.linspace(-100,100,NumTTSteps+1)
 
 #Make XAS spectra
 XEnergy = [round(x,5)*1000+.75 for x in XEnergyRaw]
@@ -75,59 +75,61 @@ for jj in range(len(UniXEnergy)):
         On_NumScan[ii][jj] = sum([int(x) for x in on])
         NormFactor_On[ii][jj] = sum(list(compress(Ipm2Sum, on)))
 
-#fig = plt.figure()
+fig = plt.figure()
 
 XASOn_Norm = [[0 for x in range(len(UniXEnergy))] for y in range(len(TTSteps)-1)]
-
-AtleastOne = [x > 0 for x in Off_NumScan]
-
-for ii in range(NumTTSteps):
-    AtleastOne = [a and sum(b) > 0 for a,b in zip(AtleastOne, On_NumScan[ii])]
 
 LegendLabel = []
 
 for ii in range(NumTTSteps):
+    for jj in range(len(UniXEnergy)):
+        
+        if NormFactor_On[ii][jj] == 0:
+            XASOn_Norm[ii][jj] = 0
+        else:
+            XASOn_Norm[ii][jj] = XASOn[ii][jj]/NormFactor_On[ii][jj]
 
-    XASOn_Norm[ii] = [a/b for a,b,c in zip(XASOn[ii], NormFactor_On[ii], AtleastOne) if c]
-    #XASOn_Norm[ii] = [a/b for a,b,c in zip(XASOn[ii], On_NumScan[ii], AtleastOne) if c]
-    LegendLabel = LegendLabel + plt.plot(list(compress(UniXEnergy, AtleastOne)), XASOn_Norm[ii])
+    LegendLabel = LegendLabel + plt.plot(UniXEnergy, XASOn_Norm[ii])
     
-#plt.xlabel('x-ray energy (keV)')
-#plt.ylabel('x-ray absorption')
+plt.xlabel('x-ray energy (keV)')
+plt.ylabel('x-ray absorption')
 
-XASOff_Norm = [a/b for a,b,c in zip(XASOff, NormFactor_Off, AtleastOne) if c]
+XASOff_Norm = [0 for x in range(len(UniXEnergy))]
 
-OutEnergy = list(compress(UniXEnergy, [x > 0 for x in AtleastOne]))
-
-XASDiff = [[0 for x in range(len(UniXEnergy))] for y in range(len(TTSteps)-1)]
-for ii in range(len(TTSteps)-1):
-    XASDiff[ii] = [a - b for a,b in zip(XASOn_Norm[ii], XASOff_Norm)]
-
-"""
-LegendLabel = LegendLabel + plt.plot(list(compress(UniXEnergy, AtleastOne)), XASOff_Norm)
+for ii in range(len(UniXEnergy)):
+    if NormFactor_Off[ii] == 0:
+        XASOff_Norm[ii] = 0
+    else:
+        XASOff_Norm[ii] = XASOff[ii]/NormFactor_Off[ii]
+    
+LegendLabel = LegendLabel + plt.plot(UniXEnergy, XASOff_Norm)
 plt.xlabel('x-ray energy (keV)')
 plt.ylabel('x-ray absorption')
 
 LegendWords = []
-for ii in range(NumTTStepsPlots):
+for ii in range(NumTTSteps):
     LegendWords = LegendWords + [str(round(TTSteps[ii],0)) + ' to ' + str(round(TTSteps[ii+1],0)) + ' fs delay']
 
 LegendWords = LegendWords + ['Off']
 
 plt.legend(LegendLabel, LegendWords)
 
+XASDiff = [[0 for x in range(len(UniXEnergy))] for y in range(len(TTSteps)-1)]
+for ii in range(len(TTSteps)-1):
+    XASDiff[ii] = [a - b for a,b in zip(XASOn_Norm[ii], XASOff_Norm)]
+
+
 LegendLabel = []
 plt.figure()
-for ii in range(NumTTStepsPlots):
+
+for ii in range(NumTTSteps):
     
-    LegendLabel = LegendLabel + plt.plot(list(compress(UniXEnergy, [x > 0 and y > 0 for x,y in zip(On_NumScan[ii], AtleastOne)])), [a - b for a,b in zip(XASOn_Norm[ii], XASOff_Norm)], marker='.')
-    #plt.plot(UniXEnergy, [a/sum(XESOn[ii]) - b/sum(XESOff) for a,b, in zip(XESOn[ii], XESOff)])
+    LegendLabel = LegendLabel + plt.plot(UniXEnergy, XASDiff[ii], marker='.')
     
 plt.xlabel('x-ray energy (keV)')
 plt.ylabel('difference in x-ray absorption (on-off)')
 
-LegendWords.pop()
+#LegendWords.pop()
 
 plt.legend(LegendLabel, LegendWords)
 
-"""
